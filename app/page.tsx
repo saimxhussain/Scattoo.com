83ab17f4ab594e68c1ad58474c85394f9f137f85
Saim Hussain <saimxhussain@gmail.com>
2026-04-19 21:33:37 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,16 @@ import Services from '@/components/Services'
 import Pricing from '@/components/Pricing'
 import CTA from '@/components/CTA'
 import Footer from '@/components/Footer'
+import FloatingCTA from '@/components/FloatingCTA'
+import PageLoader from '@/components/PageLoader'
 
 export default function Home() {
   return (
     <>
+      <PageLoader />
       <Cursor />
       <Navbar />
+      <FloatingCTA />
       <main>
         <Hero />
         <Ticker />
